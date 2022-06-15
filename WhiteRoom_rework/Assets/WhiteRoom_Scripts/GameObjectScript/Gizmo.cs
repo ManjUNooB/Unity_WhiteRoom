accf66331b03e82f72e03da84fbf53bfb12184d7
ManjUNooB <ryuzin20000901@gmail.com>
2022-06-15 11:34:17 +0900

    ProBuilderを使用しての部屋の作成 チュートリアルステージの作成 ドアの仮作成 Gizmoの作成

---
+++ WhiteRoom_rework/Assets/WhiteRoom_Scripts/GameObjectScript/Gizmo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    [SerializeField]private float gizmoSize = 0.3f;
+    [SerializeField]private Color gizmoColor = Color.yellow;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+}
